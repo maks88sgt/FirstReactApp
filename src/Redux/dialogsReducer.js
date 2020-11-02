7be3152f869f3e4c606d2ced235b6587d75f11d3
maks88sgt <maks88sgt@yandex.ru>
2020-11-02 17:36:01 +0300

    Reducers (refactor with "switch..case"

--- src/Redux/dialogsReducer.js
+++ src/Redux/dialogsReducer.js
@@ -2,12 +2,17 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
 export const dialogsReducer = (state, action) => {
-    if (action.type === UPDATE_NEW_MESSAGE_TEXT){
-        state.newMessageText = action.message;
-    } else if (action.type === SEND_MESSAGE) {
-        let newMessage = state.newMessageText;
-        state.newMessageText = '';
-        state.messages.push({id:4, message: newMessage});
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.message;
+            return state;
+        case SEND_MESSAGE:
+            let newMessage = state.newMessageText;
+            state.newMessageText = '';
+            state.messages.push({id: 4, message: newMessage});
+            return state;
+        default:
+            return state;
     }
-    return state;
+
 }
--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -3,16 +3,20 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 
 export const profileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 4,
-            post: state.newPostText,
-            likesCount: 0,
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 4,
+                post: state.newPostText,
+                likesCount: 0,
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
     }
-    return state;
 };
