c047b76301ae2e07b8029223237720467a31128b
maks88sgt <maks88sgt@yandex.ru>
2020-11-04 17:25:24 +0300

    Some refactor reducers

--- src/Redux/dialogsReducer.js
+++ src/Redux/dialogsReducer.js
@@ -2,37 +2,39 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
 let initialState = {
-        dialogs: [
-            {id: 1, name: 'Egor'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Maks'},
-        ],
-        messages: [
-            {id: 1, message: 'Hi!'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Let\'s go to the circus'},
-        ],
-        newMessageText: '',
-    };
+    dialogs: [
+        {id: 1, name: 'Egor'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Maks'},
+    ],
+    messages: [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Let\'s go to the circus'},
+    ],
+    newMessageText: '',
+};
 
 export const dialogsReducer = (state = initialState, action) => {
-    let stateCopy =  {
-        ...state,
-        //messages: [...state.messages],
-    };
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_TEXT:
-            stateCopy.newMessageText = action.message;
-            return stateCopy;
+            return {
+                ...state,
+                newMessageText: action.message,
+            };
+
         case SEND_MESSAGE:
-            let newMessage = stateCopy.newMessageText;
-            stateCopy.newMessageText = '';
-            stateCopy.messages.push({id: 4, message: newMessage});
-            return stateCopy;
+            let newMessage = state.newMessageText;
+            return {
+                ...state,
+                messages: [...state.messages, {id: 4, message: newMessage}],
+                newMessageText:'',
+            };
+
         default:
             return state;
     }
 };
 
-export const updateNewMessageActionCreator = (text) =>({type:UPDATE_NEW_MESSAGE_TEXT, message: text,});
-export const sendMessageActionCreator = () => ({type: SEND_MESSAGE, });
+export const updateNewMessageActionCreator = (text) => ({type: UPDATE_NEW_MESSAGE_TEXT, message: text,});
+export const sendMessageActionCreator = () => ({type: SEND_MESSAGE,});
--- src/Redux/profileReducer.js
+++ src/Redux/profileReducer.js
@@ -2,33 +2,30 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
-        posts: [
-            {id: 1, post: 'Hi, how are you?', likesCount: 20},
-            {id: 2, post: 'It\'s my first post', likesCount: 11},
-            {id: 3, post: 'I finished my first task', likesCount: 5},
-        ],
-        newPostText: '',
-    };
+    posts: [
+        {id: 1, post: 'Hi, how are you?', likesCount: 20},
+        {id: 2, post: 'It\'s my first post', likesCount: 11},
+        {id: 3, post: 'I finished my first task', likesCount: 5},
+    ],
+    newPostText: '',
+};
 
 
 export const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST: {
-            let newPost = {
-                id: 4,
-                post: state.newPostText,
-                likesCount: 0,
+            return {
+                ...state,
+                posts: [...state.posts, {id: 4, post: state.newPostText, likesCount: 0},],
+                newPostText: ''
             };
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
         }
         case UPDATE_NEW_POST_TEXT:
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
+            return {
+                ...state,
+                newPostText: action.newText
+            };
+
         default:
             return state;
     }
